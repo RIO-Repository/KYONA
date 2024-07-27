07d299b98693eead4c44c0dfe665eaab82099b82
Pujan Sarkar <119032715+ProPujan007@users.noreply.github.com>
2024-07-28 00:31:54 +0530

    feat: server setup

--- packages/index.js
+++ packages/index.js
@@ -1,20 +1,37 @@
-const express = require('express');
-const path = require('path');
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+
+dotenv.config;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({extended: true}));
 
-// Serve static files from the frontend
-app.use(express.static(path.join(__dirname, '../dist')));
-
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from the server!' });
+app.use((err, req, res, next) =>{
+    const status = err.status || 500 ;
+    const message = err.message || "Error happened !";
+    return res.status(status).json ({
+        success: false,
+        status,
+        message,
+    });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
+
+app.get("/", async (req, res) => {
+    res.status(200).json ({
+        message: "Hey👋! Welcome to server ",
+    });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async() => {
+    try {
+        app.listen(5000, () => console.log("Server started on 5000 port"))
+    }catch (error) {
+        console.log(error);
+    }
+};
+startServer();
